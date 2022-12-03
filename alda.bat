986b90c8e9b75b37d3bfae65cacad119b418ddcb
Mohcene Mouad Lariane <76631615+LarianeMohceneMouad@users.noreply.github.com>
2022-12-03 22:04:04 +0100

    Update alda.bat

--- alda.bat
+++ alda.bat
@@ -1,7 +1,7 @@
 cd C:\Users\HP\PycharmProjects\BlockChain\
-start cmd.exe @cmd /k python p2p_boot_stable_test.py
+start cmd.exe @cmd /k python agent.py
 for /L %%a in (1,1,2) do (
-start cmd.exe @cmd /k python p2p_agent_stabe_test.py
+start cmd.exe @cmd /k python agent.py
 )
 done
 
